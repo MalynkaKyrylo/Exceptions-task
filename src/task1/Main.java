dc589ae0a6b15899beb1e85edc0be61b81b9e2cb
Malynka Kyrylo <kirillmalinka@gmail.com>
2023-07-06 21:46:04 +0300

    Solution task 1

--- src/task1/Main.java
+++ src/task1/Main.java
@@ -1,7 +1,26 @@
 package task1;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        getOutput(getData());
+    }
 
+    private static int getData() {
+        try {
+            Scanner scanner = new Scanner(System.in);
+            int num = 0;
+            System.out.println("Enter the number:");
+            num = Integer.parseInt(scanner.nextLine());
+            return num;
+        } catch (NumberFormatException e) {
+            System.out.println("Wrong value! Please enter the number:");
+            return getData();
+        }
+    }
+
+    private static void getOutput(int num) {
+        System.out.println("The number is " + num);
     }
 }
